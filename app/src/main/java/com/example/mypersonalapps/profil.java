eaa1de83546445bd1b52bc90c965fe047cfb5b08
afifr <afifrizky186@gmail.com>
2019-05-21 23:57:00 +0700

    hello world

---
+++ app/src/main/java/com/example/mypersonalapps/profil.java
@@ -0,0 +1,22 @@
+package com.example.mypersonalapps;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+// 21 Mei 2019, 10116168, Afif Rizky Darmawan, IF-4 / Komputasi 4
+
+public class profil extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_profil, container, false);
+
+        return view;
+    }
+}
